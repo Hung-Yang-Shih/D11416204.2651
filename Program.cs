f8a7fa307c9b6182f76103591979867dffed076b
Administrator <Administrator@A303-7-02>
2025-10-08 12:02:04 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace D11416204
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("輸入抵達時間");
+                int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("輸入遲到時間");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("時間");
+            Console.WriteLine(findDelayedArrivalTime(a, b));
+            int findDelayedArrivalTime(int arrivalTime, int delayedTime)
+            {
+                //2651
+                //1.輸入
+                //int arrivalTime, delayedTime
+
+                //2.處理
+                int comput;
+                comput = arrivalTime + delayedTime;
+                comput = comput % 24;
+                //3.輸出
+                int result;
+                result = comput;
+                return result;
+            }
+        }
+    }
+}
